7ec8e4d0879bf97f1cbb8366c46bf0fe2accb10b
Rafael Castillo <rcastill@redhat.com>
2022-04-27 14:18:21 -0700

    Update project module to be compatible with new sdk
    - Change sdk calls to use proxy objects
    - Convert return values to dict before updating
    - Adds additional test values
    
    Change-Id: I187a27af4a5b8aa7cd4b60a1a876b5e5e6975144

--- plugins/modules/project.py
+++ plugins/modules/project.py
@@ -14,37 +14,38 @@ description:
       The value for I(name) cannot be updated without deleting and
       re-creating the project.
 options:
-   name:
-     description:
-        - Name for the project
-     required: true
-     type: str
-   description:
-     description:
-        - Description for the project
-     type: str
-   domain_id:
-     description:
-        - Domain id to create the project in if the cloud supports domains.
-     aliases: ['domain']
-     type: str
-   enabled:
-     description:
-        - Is the project enabled
-     type: bool
-     default: 'yes'
-   properties:
-     description:
-        - Additional properties to be associated with this project. Requires
-          openstacksdk>0.45.
-     type: dict
-     required: false
-   state:
-     description:
-       - Should the resource be present or absent.
-     choices: [present, absent]
-     default: present
-     type: str
+  name:
+    description:
+      - Name for the project
+    required: true
+    type: str
+  description:
+    description:
+      - Description for the project
+    type: str
+  domain:
+    description:
+       - Domain name or id to create the project in if the cloud supports
+         domains.
+    aliases: ['domain_id']
+    type: str
+  is_enabled:
+    description:
+      - Is the project enabled
+    aliases: ['enabled']
+    type: bool
+    default: 'yes'
+  properties:
+    description:
+      - Additional properties to be associated with this project.
+    type: dict
+    required: false
+  state:
+    description:
+      - Should the resource be present or absent.
+    choices: [present, absent]
+    default: present
+    type: str
 requirements:
     - "python >= 3.6"
     - "openstacksdk"
@@ -61,8 +62,8 @@ EXAMPLES = '''
     state: present
     name: demoproject
     description: demodescription
-    domain_id: demoid
-    enabled: True
+    domain: demoid
+    is_enabled: True
     properties:
       internal_alias: demo_project
 
@@ -79,24 +80,40 @@ RETURN = '''
 project:
     description: Dictionary describing the project.
     returned: On success when I(state) is 'present'
-    type: complex
+    type: dict
     contains:
-        id:
-            description: Project ID
-            type: str
-            sample: "f59382db809c43139982ca4189404650"
-        name:
-            description: Project name
-            type: str
-            sample: "demoproject"
         description:
             description: Project description
             type: str
             sample: "demodescription"
-        enabled:
-            description: Boolean to indicate if project is enabled
+        domain_id:
+            description: domain to which the project belongs
+            type: str
+            sample: "default"
+        id:
+            description: Project ID
+            type: str
+            sample: "f59382db809c43139982ca4189404650"
+        is_domain:
+            description: Indicates whether the project also acts as a domain.
             type: bool
-            sample: True
+        is_enabled:
+            description: Indicates whether the project is enabled
+            type: bool
+        name:
+            description: Project name
+            type: str
+            sample: "demoproject"
+        options:
+            description: The resource options for the project
+            type: dict
+        parent_id:
+            description: The ID of the parent of the project
+            type: str
+        tags:
+            description: A list of associated tags
+            type: list
+            elements: str
 '''
 
 from ansible_collections.openstack.cloud.plugins.module_utils.openstack import OpenStackModule
@@ -106,109 +123,96 @@ class IdentityProjectModule(OpenStackModule):
     argument_spec = dict(
         name=dict(required=True),
         description=dict(required=False),
-        domain_id=dict(required=False, aliases=['domain']),
+        domain=dict(required=False, aliases=['domain_id']),
+        is_enabled=dict(default=True, type='bool', aliases=['enabled']),
         properties=dict(required=False, type='dict', min_ver='0.45.1'),
-        enabled=dict(default=True, type='bool'),
         state=dict(default='present', choices=['absent', 'present'])
     )
     module_kwargs = dict(
         supports_check_mode=True
     )
 
-    def _needs_update(self, project):
-        keys = ('description', 'enabled')
-        for key in keys:
-            if self.params[key] is not None and self.params[key] != project.get(key):
-                return True
+    def _needs_update(self, project, update, extra):
+        # We cannot update a project name because name find projects by name so
+        # only a project with an already matching name will be considered for
+        # updates
+        keys = ('description', 'is_enabled')
+        if any((k in update and update[k] != project[k]) for k in keys):
+            return True
 
-        properties = self.params['properties']
-        if properties:
-            project_properties = project.get('properties')
-            for k, v in properties.items():
-                if v is not None and (k not in project_properties or v != project_properties[k]):
-                    return True
+        # Additional keys passed by user will be checked completely
+        if extra and any(k not in project or extra[k] != project[k]
+                         for k in extra.keys()):
+            return True
 
         return False
 
-    def _system_state_change(self, project):
-        state = self.params['state']
+    def _get_domain_id(self, domain):
+        dom_obj = self.conn.identity.find_domain(domain)
+        if dom_obj is None:
+            # Ok, let's hope the user is non-admin and passing a sane id
+            return domain
+        return dom_obj.id
+
+    def _system_state_change(self, state, project, attrs, extra_attrs):
         if state == 'present':
             if project is None:
-                changed = True
-            else:
-                if self._needs_update(project):
-                    changed = True
-                else:
-                    changed = False
-
-        elif state == 'absent':
-            changed = project is not None
-
-        return changed
+                return True
+            return self._needs_update(project, attrs, extra_attrs)
+        # Else state is absent
+        return project is not None
 
     def run(self):
         name = self.params['name']
-        description = self.params['description']
-        domain = self.params['domain_id']
-        enabled = self.params['enabled']
-        properties = self.params['properties'] or {}
+        domain = self.params['domain']
         state = self.params['state']
+        properties = self.params['properties']
+        enabled = self.params['is_enabled']
+        description = self.params['description']
 
+        find_project_kwargs = {}
+        domain_id = None
         if domain:
-            try:
-                # We assume admin is passing domain id
-                dom = self.conn.get_domain(domain)['id']
-                domain = dom
-            except Exception:
-                # If we fail, maybe admin is passing a domain name.
-                # Note that domains have unique names, just like id.
-                try:
-                    dom = self.conn.search_domains(filters={'name': domain})[0]['id']
-                    domain = dom
-                except Exception:
-                    # Ok, let's hope the user is non-admin and passing a sane id
-                    pass
+            domain_id = self._get_domain_id(domain)
+            find_project_kwargs['domain_id'] = domain_id
 
-        if domain:
-            project = self.conn.get_project(name, domain_id=domain)
-        else:
-            project = self.conn.get_project(name)
+        project = None
+        if name is not None:
+            project = self.conn.identity.find_project(
+                name, **find_project_kwargs)
 
-        if self.ansible.check_mode:
-            self.exit_json(changed=self._system_state_change(project))
+        project_attrs = {
+            'name': name,
+            'description': description,
+            'is_enabled': enabled,
+            'domain_id': domain_id,
+        }
+        project_attrs = {k: v for k, v in project_attrs.items()
+                         if v is not None}
+        # Add in arbitrary properties
+        if properties:
+            project_attrs.update(properties)
 
+        if self.check_mode:
+            self.exit_json(changed=self._system_state_change(state, project,
+                                                             project_attrs,
+                                                             properties))
+
+        changed = False
         if state == 'present':
             if project is None:
-                project = self.conn.create_project(
-                    name=name, description=description,
-                    domain_id=domain,
-                    enabled=enabled)
+                project = self.conn.identity.create_project(**project_attrs)
                 changed = True
-
-                project = self.conn.update_project(
-                    project['id'],
-                    description=description,
-                    enabled=enabled,
-                    **properties)
-            else:
-                if self._needs_update(project):
-                    project = self.conn.update_project(
-                        project['id'],
-                        description=description,
-                        enabled=enabled,
-                        **properties)
-                    changed = True
-                else:
-                    changed = False
-            self.exit_json(changed=changed, project=project)
-
-        elif state == 'absent':
-            if project is None:
-                changed = False
-            else:
-                self.conn.delete_project(project['id'])
+            elif self._needs_update(project, project_attrs, properties):
+                project = self.conn.identity.update_project(
+                    project, **project_attrs)
                 changed = True
-            self.exit_json(changed=changed)
+            self.exit_json(changed=changed,
+                           project=project.to_dict(computed=False))
+        elif state == 'absent' and project is not None:
+            self.conn.identity.delete_project(project['id'])
+            changed = True
+        self.exit_json(changed=changed)
 
 
 def main():
