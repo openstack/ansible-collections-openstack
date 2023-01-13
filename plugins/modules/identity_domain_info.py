16a8a9e5d4444278ca5c81be091de55e30f4ad81
Jakob Meng <code@jakobmeng.de>
2023-01-13 09:05:57 +0100

    Refactored identity_domain{,_info} modules
    Change-Id: Idf48f10e66a5651fa4693774eecd2c8683703082

--- plugins/modules/identity_domain_info.py
+++ plugins/modules/identity_domain_info.py
@@ -4,82 +4,67 @@
 # Copyright (c) 2016 Hewlett-Packard Enterprise Corporation
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: identity_domain_info
-short_description: Retrieve information about one or more OpenStack domains
+short_description: Fetch identity (Keystone) domains from OpenStack cloud
 author: OpenStack Ansible SIG
 description:
-    - Retrieve information about a one or more OpenStack domains
+  - Fetch identity (Keystone) domains from OpenStack cloud
 options:
-   name:
-     description:
-        - Name or ID of the domain
-     type: str
-   filters:
-     description:
-        - A dictionary of meta data to use for filtering. Elements of
-          this dictionary may be additional dictionaries.
-     type: dict
+  filters:
+    description:
+      - A dictionary of meta data to use for filtering.
+      - Elements of this dictionary may be additional dictionaries.
+    type: dict
+  name:
+    description:
+      - Name or ID of the domain
+    type: str
 extends_documentation_fragment:
-- openstack.cloud.openstack
+  - openstack.cloud.openstack
 '''
 
-EXAMPLES = '''
-# Gather information about previously created domain
-- openstack.cloud.identity_domain_info:
+EXAMPLES = r'''
+- name: Gather information about previously created domain
+  openstack.cloud.identity_domain_info:
     cloud: awesomecloud
-  register: result
-- debug:
-    msg: "{{ result.openstack_domains }}"
 
-# Gather information about a previously created domain by name
-- openstack.cloud.identity_domain_info:
+- name: Gather information about a previously created domain by name
+  openstack.cloud.identity_domain_info:
     cloud: awesomecloud
     name: demodomain
-  register: result
-- debug:
-    msg: "{{ result.openstack_domains }}"
 
-# Gather information about a previously created domain with filter
-- openstack.cloud.identity_domain_info:
+- name: Gather information about a previously created domain with filter
+  openstack.cloud.identity_domain_info:
     cloud: awesomecloud
     name: demodomain
     filters:
-      enabled: false
-  register: result
-- debug:
-    msg: "{{ result.openstack_domains }}"
+      is_enabled: false
 '''
 
-
-RETURN = '''
-openstack_domains:
-    description: has all the OpenStack information about domains
-    returned: always, but can be null
-    type: list
-    elements: dict
-    contains:
-        id:
-            description: Unique UUID.
-            returned: success
-            type: str
-        name:
-            description: Name given to the domain.
-            returned: success
-            type: str
-        description:
-            description: Description of the domain.
-            returned: success
-            type: str
-        is_enabled:
-            description: Flag to indicate if the domain is enabled.
-            returned: success
-            type: bool
-        links:
-            type: list
-            returned: success
-            description: The links related to the domain resource
+RETURN = r'''
+domains:
+  description: List of dictionaries describing OpenStack domains
+  returned: always
+  type: list
+  elements: dict
+  contains:
+    description:
+      description: Description of the domain.
+      type: str
+    id:
+      description: Unique UUID.
+      type: str
+    is_enabled:
+      description: Flag to indicate if the domain is enabled.
+      type: bool
+    links:
+      description: The links related to the domain resource
+      type: list
+    name:
+      description: Name given to the domain.
+      type: str
 '''
 
 from ansible_collections.openstack.cloud.plugins.module_utils.openstack import OpenStackModule
@@ -87,8 +72,8 @@ from ansible_collections.openstack.cloud.plugins.module_utils.openstack import O
 
 class IdentityDomainInfoModule(OpenStackModule):
     argument_spec = dict(
-        name=dict(),
         filters=dict(type='dict'),
+        name=dict(),
     )
 
     module_kwargs = dict(
@@ -96,16 +81,18 @@ class IdentityDomainInfoModule(OpenStackModule):
     )
 
     def run(self):
+        kwargs = {}
         name = self.params['name']
-        filters = self.params['filters'] or {}
+        if name is not None:
+            kwargs['name_or_id'] = name
 
-        args = {}
-        if name:
-            args['name_or_id'] = name
-        args['filters'] = filters
+        filters = self.params['filters']
+        if filters is not None:
+            kwargs['filters'] = filters
 
-        domains = [d.to_dict(computed=False) for d in self.conn.search_domains(**args)]
-        self.exit_json(changed=False, openstack_domains=domains)
+        self.exit_json(changed=False,
+                       domains=[d.to_dict(computed=False)
+                                for d in self.conn.search_domains(**kwargs)])
 
 
 def main():
